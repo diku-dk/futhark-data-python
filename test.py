ffb575d910668ad06c8456fd8f47fb7c08a995c8
Troels Henriksen <athas@sigkill.dk>
2021-08-07 17:27:20 +0200

    Lint fix.

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 # Used by test.sh.
 
-import futhark_data
 import io
 import sys
+
+import futhark_data
 
 for v in futhark_data.load(open(sys.argv[1], 'rb')):
     sys.stdout.buffer.write(futhark_data.dumpb(v))
